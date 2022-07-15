fe72482e8b0fc0fe5b2758d292b700616b6fb098
Aliaksandr <alexander.satsunkevich@gmail.com>
2022-07-15 11:18:57 +0400

    feat: gh pages set public folder

--- src/models/filtering-options.ts
+++
@@ -1,9 +0,0 @@
-export class FilteringOptions{
-  cartCount!: string;
-  yearSlider!: string[];
-  countSlider!: string[];
-  isPopular!: boolean;
-  colorFilter!: { id: string; checked: boolean; }[];
-  sizeFilter!: { id: string; checked: boolean; }[];
-  producerFilter!: { id: string; checked: boolean; }[]
-}
